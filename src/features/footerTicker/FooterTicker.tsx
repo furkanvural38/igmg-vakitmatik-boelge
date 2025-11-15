9b1bce4a9a473c57c1e3a8cb6aa1e30760246df1
Furkan Vural <furkan.vural@hotmail.de>
2025-11-16 00:26:38 +0100

    fix scroll

--- src/features/footerTicker/FooterTicker.tsx
+++ src/features/footerTicker/FooterTicker.tsx
@@ -171,7 +171,7 @@ export function FooterTicker() {
                                 maxWidth: "100%",
                             }}
                         >
-                            <div className="text-white text-center mt-16" style={{ fontSize: "6rem", lineHeight: 1.2 }}>
+                            <div className="text-white text-center mt-4" style={{ fontSize: "6rem", lineHeight: 1.2 }}>
                                 {activeItem.text}
                             </div>
 
